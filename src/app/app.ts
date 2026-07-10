d84136e7dc17521a0592d85695ed417d48a62ebf
NandoJunioDev <frnndmaz_cf@outlook.com>
2026-07-09 22:50:14 -0300

    style: header feita 09/07, inicio do projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero-component/hero-component/hero-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
