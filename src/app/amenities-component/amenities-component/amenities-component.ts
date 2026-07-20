7f3b790ff0e5bf7f621b0721e504c50c6f1c06a0
NandoJunioDev <frnndmaz_cf@outlook.com>
2026-07-20 00:04:03 -0300

    feat (gallery-amentities) [ foi adicionado galeria, com fotos, e imformação sobre a embarcarção

---
+++ src/app/amenities-component/amenities-component/amenities-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amenities-component',
+  imports: [],
+  templateUrl: './amenities-component.html',
+  styleUrl: './amenities-component.css',
+})
+export class AmenitiesComponent {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero-component/hero-component/hero-component';
+import {AmenitiesComponent} from './amenities-component/amenities-component/amenities-component';
+import {GalleryComponent} from './gallery-component/gallery-component/gallery-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent],
+  imports: [RouterOutlet, HeroComponent, AmenitiesComponent, GalleryComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
